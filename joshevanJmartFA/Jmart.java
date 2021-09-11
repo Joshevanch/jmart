30ada9ecd979bd3a7ee228cca80b10065678ada3
Joshevan <joshevan@ui.ac.id>
2021-09-11 10:34:13 +0700

    Case Study

--- joshevanJmartFA/Jmart.java
+++ joshevanJmartFA/Jmart.java
@@ -1,35 +1,59 @@
 package joshevanJmartFA;
 
 
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main (String[] args){
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public static int getPromo (){
+        return 0;
+    }
+    
+    public static String getCustomer(){
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage (int before,int after){
+        if (before <= after){
+            return 0;
+        }
+        else {
+            return ( (float)(before - after) / before*100);
+        }
+    }
+    
+    public static int getDiscountedPrice (int price, float discountPercentage){
+        if (discountPercentage > 100){
+            discountPercentage = 100;
+        }
+            price = price - (int)(price * discountPercentage/100);
+        return price;
+    }
+    
+    public static int getOriginalPrice (int discountedPrice, float discountPercentage){
+        if (discountPercentage == 0){
+            return 0;
+        }
+        else {
+        discountedPrice = discountedPrice / (int)(100-discountPercentage) * 100;
+        return discountedPrice;
+    }
+    }
+    
+    public static float getCommisionMultiplier (){
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice  (int price){
+        price = price + (int)(price * getCommisionMultiplier());
+        return price;
+    }
+    
+    public static int getAdminFee (int price){
+        price = (int)(price * getCommisionMultiplier());
+        return price;
     }
 }
