af6eb60b12d94f7f02c085936ba17b4a29d86aaa
Joshevan <joshevan@ui.ac.id>
2021-09-18 10:56:36 +0700

    CS modul 2 - adding some new classes

--- joshevanJmartFA/Jmart.java
+++ joshevanJmartFA/Jmart.java
@@ -5,13 +5,7 @@ package joshevanJmartFA;
 public class Jmart
 {
     public static void main (String[] args){
-        Product test = Jmart.create();
-        System.out.println(test.id);
-        System.out.println(test.name);
-        System.out.println(test.weight);
-        System.out.println(test.conditionUsed);
-        System.out.println(test.priceTag.getAdjustedPrice());
-        System.out.println(test.category);
+        
     }
     public static Product create(){
         PriceTag hargabaju = new PriceTag (100000, 10);
