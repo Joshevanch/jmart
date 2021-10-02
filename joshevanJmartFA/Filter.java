2bda1ca0f133ebff168f8a224255b5637b5f5f0c
Joshevan <joshevan@ui.ac.id>
2021-10-02 11:07:10 +0700

    CS Modul 4

--- joshevanJmartFA/Filter.java
+++ joshevanJmartFA/Filter.java
@@ -4,16 +4,16 @@ public class Filter
 {
     public static ArrayList <PriceTag> filterPriceTag (PriceTag[] list, int value, boolean less){
         ArrayList <PriceTag> priceTags = new ArrayList <PriceTag>();
-        if ( less = true){
+        if (less == true){
             for (PriceTag num : list){
                 if (num.getAdjustedPrice() < value){
                     priceTags.add(num);
                 }
             }
         }
-        if ( less = false){
+        if ( less == false){
             for (PriceTag num : list){
-                if (num.getAdjustedPrice() > value){
+                if (num.getAdjustedPrice() >= value){
                      priceTags.add(num);
                 }
             }
@@ -21,6 +21,20 @@ public class Filter
     return  priceTags;
 }
     public static void filterProductRating (ArrayList <ProductRating> list,double value, boolean less){
-        
+         ArrayList <ProductRating> productRating = new ArrayList <ProductRating>();
+        if (less == true ){
+            for (ProductRating num : list){
+                if (num.getAverage() >= value){
+                    productRating.add(num);
+                }
+            }
+        }
+        if (less == false ){
+            for (ProductRating num : list){
+                if (num.getAverage() < value){
+                    productRating.add(num);
+                }
+            }
+        }
     }
 }
